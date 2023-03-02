c8a7d543c1e13a4ca315740d91836fef270a2e85
perrineosw <oswald.perrinedu40@gmail.com>
2023-03-02 22:49:03 +0100

    Updates

--- src/pages/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotFound = (): JSX.Element => {
-    return (
-        <div className="bg-gray-200">Hello, Tailwind CSS!</div>
-    )
-}
-
-export default NotFound
