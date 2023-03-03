1639ede3832c3beb827abda3628b32781134affc
perrineosw <oswald.perrinedu40@gmail.com>
2023-03-03 09:36:11 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import './custom.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -16,7 +16,7 @@ import NotFound from './pages/NotFound';
 const App = () => {
     return (
         <div className="bg-homeBg min-h-screen bg-no-repeat bg-center bg-cover bg-fixed md:pb-16 w-full custom-class">
-            <Router basename="/Portfolio">
+            <BrowserRouter basename="/Portfolio">
                 <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -28,8 +28,7 @@ const App = () => {
                     <Route path="*" element={<NotFound />} />
                 </Routes>
                 {(window.location.pathname !== '/home') ? <Footer /> : ""}
-
-            </Router>
+            </BrowserRouter>
         </div>
     );
 };
