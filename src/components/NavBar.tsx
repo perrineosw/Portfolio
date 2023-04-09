a4db2f77699d45382dd714fc042e0e6dbfcb3228
perrineosw <oswald.perrinedu40@gmail.com>
2023-04-10 00:53:35 +0200

    navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import React from "react";
+import React, { useState } from 'react';
+
 import {NavLink} from "react-router-dom";
-import {faList, faHouse, faUser, faListDots, faSuitcase, faFile} from '@fortawesome/free-solid-svg-icons';
+import {faSun, faMoon, faList, faHouse, faUser, faListDots, faSuitcase, faFile} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const NavBar = ({toggleMode, isDarkMode}: {toggleMode: () => void, isDarkMode: boolean}) => {
@@ -17,12 +17,15 @@ const NavBar = ({toggleMode, isDarkMode}: {toggleMode: () => void, isDarkMode: b
         <header className="flex py-5 justify-between items-center px-10 bg-gray-300 fixed top-0 left-0 bg-transparent lg:static z-[1111111111]">
             <div className="flex">
                 <div className="flex justify-between w-full items-center space-x-4">
+                    <button className="menu-item-two" onClick={toggleMode}>
+                        {isDarkMode ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+                    </button>
                     <div className="flex items-center">
                         <button id="menu-toggle" type="button" className="menu-toggle-btn" onClick={() => setOpen(!open)}>
-                            <FontAwesomeIcon icon={faList} />
+                            <FontAwesomeIcon icon={faList}/>
                         </button>
                         <nav id="navbar" className={`flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row ${open ? 'flex' : 'hidden'}`}>
-                            <ul className="block rounded-b-[20px] shadow-md absolute left-0 top-20 z-[22222222222222] w-full bg-white">
+                            <ul className="block rounded-b-[20px] shadow-md absolute left-0 top-20 z-[22222222222222] bg-white mobile-ul-size">
                                 <li>
                                     <NavLink to="/home" className={(window.location.hash === '#/home' || window.location.hash === '') ? "mobile-menu-items-active" : "mobile-menu-items"} onClick={() => handleLinkClick('#/home')}>
                                         <FontAwesomeIcon icon={faHouse} />&nbsp;&nbsp;&nbsp;&nbsp;Accueil
@@ -54,7 +57,6 @@ const NavBar = ({toggleMode, isDarkMode}: {toggleMode: () => void, isDarkMode: b
                     </div>
                     <nav className="hidden lg:block">
                         <ul className="flex">
-                            <li><button onClick={toggleMode}>Toggle Mode</button></li>
                             <li>
                                 <NavLink to="/home" className={(window.location.hash === '#/home' || window.location.hash === '') ? "menu-item-two-active" : "menu-item-two"} onClick={() => handleLinkClick('#/home')}>
                                     <FontAwesomeIcon icon={faHouse} />&nbsp;&nbsp;&nbsp;&nbsp;Accueil
